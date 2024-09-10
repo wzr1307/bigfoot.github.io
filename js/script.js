67e76fb509e617eeb71222239d73fa544f6bba93
wzr1307 <94065069+wzr1307@users.noreply.github.com>
2024-09-10 17:58:15 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
-const registerForm = document.getElementById('registerForm');
-registerForm.addEventListener('submit', (event) => {
-  event.preventDefault();
-  // Здесь могла бы быть логика сохранения данных, но мы ее опустим
-  alert('Регистрация прошла успешно!');
-  window.location.href = 'register.html'; // Переход на главную страницу
+const signInButton = document.querySelector('.sign-in-button');
+const signUpButton = document.querySelector('.sign-up-button');
+const formContainer = document.querySelector('.registration-form');
+const confirmPasswordField = document.querySelector('#confirmPass');
+
+signInButton.addEventListener('click', () => {
+    formContainer.classList.remove('slide-right');
+    formContainer.classList.add('slide-left');
+    confirmPasswordField.style.display = 'none';
+});
+
+signUpButton.addEventListener('click', () => {
+    formContainer.classList.remove('slide-left');
+    formContainer.classList.add('slide-right');
+    confirmPasswordField.style.display = 'block';
 });
